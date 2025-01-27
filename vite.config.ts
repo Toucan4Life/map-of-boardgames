22877df72031decdf2e387bf92612f1a23a8bb31
Toucan4Life <tdp94@hotmail.com>
2025-01-27 13:56:41 +0000

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  // resolve: {
-  //   alias: {
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   //needed because of this bug https://github.com/vitejs/vite/issues/16522
   server: { host: '127.0.0.1' },
 })
